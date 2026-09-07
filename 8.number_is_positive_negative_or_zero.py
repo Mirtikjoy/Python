53314f44bbc0b4f22c0483cabec35aef727fa94e
Mirtik Joy Molsom <mirtikjoymolsom90@gmail.com>
2026-09-07 19:15:42 +0530

    file

---
+++ 8.number_is_positive_negative_or_zero.py
@@ -0,0 +1,8 @@
+num = int(input("please enter your number: "))
+
+if num == 0:
+    print("The number is Zero:",num)
+elif num < 0:
+    print("The number is negavtive number", num)
+else:
+    print("THe number is positive",num)
